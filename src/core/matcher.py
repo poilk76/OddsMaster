b49510823a7925e08bbc361265e220c9d23303b4
poilk <bartekchmiel439@gmail.com>
2026-08-02 09:44:22 +0200

    modified:   src/core/matcher.py

--- src/core/matcher.py
+++ src/core/matcher.py
@@ -2,18 +2,21 @@ from core.normalizer import normalize
 from rapidfuzz.fuzz import ratio
 from typing import Tuple
 
-def are_names_matching(text1:str,text2:str) -> bool:
+def are_names_matching(text1:str,text2:str,pass_treashold:int = 80) -> bool:
 
     if len(text1) == 0 or len(text2) == 0: return False
 
     text1_normalized = normalize(text1)
     text2_normalized = normalize(text2)
 
-    return ratio(text1_normalized,text2_normalized) > 80
+    return ratio(text1_normalized,text2_normalized) > pass_treashold
 
 MATCH_KEYS = ["team1","team2","start","category"]
 
-def are_matches_matching(match1,match2) -> Tuple[bool,bool]:
+def are_matches_matching(match1,match2,
+                         pass_treashold:int=80,
+                         time_distance_allowed_minuts:int=30
+                         ) -> Tuple[bool,bool]:
     """
     match format:
     {
@@ -35,13 +38,13 @@ def are_matches_matching(match1,match2) -> Tuple[bool,bool]:
        not match1['start'] == match2['start']:
         return (False,False)
 
-    if are_names_matching(match1['team1'],match2['team1']) and\
-       are_names_matching(match1['team2'],match2['team2']):
+    if are_names_matching(match1['team1'],match2['team1'],pass_treashold) and\
+       are_names_matching(match1['team2'],match2['team2'],pass_treashold):
 
         return (True,False)
 
-    if are_names_matching(match1['team1'],match2['team2']) and\
-       are_names_matching(match1['team2'],match2['team1']):
+    if are_names_matching(match1['team1'],match2['team2'],pass_treashold) and\
+       are_names_matching(match1['team2'],match2['team1'],pass_treashold):
     
         return (True,True)
 
